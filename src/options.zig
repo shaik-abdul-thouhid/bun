e65e9deb6d7c196c6cc9548bddd0a36fd1e2ff8b
pfg <pfg@pfg.pw>
2025-01-22 16:35:05 -0800

    revert attempt to remove fn loader()

--- src/options.zig
+++ src/options.zig
@@ -1670,9 +1670,7 @@ pub const BundleOptions = struct {
         this.defines_loaded = true;
     }
 
-    pub const loader = loaderForExtension;
-    /// loader from {type: ""} import attribute must be preferred over the file extension
-    pub fn loaderForExtension(this: *const BundleOptions, ext: string) Loader {
+    pub fn loader(this: *const BundleOptions, ext: string) Loader {
         return this.loaders.get(ext) orelse .file;
     }
 
